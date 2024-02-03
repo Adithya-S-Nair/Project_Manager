45c79b8427903e3fb335708f23cf8d71695b0b9f
p111222 <princejainpre.ram@gmail.com>
2024-02-03 14:54:33 +0530

    form completed

--- client/src/Components/FloatingActionButtonComponent.jsx
+++ client/src/Components/FloatingActionButtonComponent.jsx
@@ -5,9 +5,13 @@ import SpeedDialAction from '@mui/material/SpeedDialAction';
 import NewspaperIcon from '@mui/icons-material/Newspaper';
 import ArticleIcon from '@mui/icons-material/Article';
 import TaskIcon from '@mui/icons-material/Task';
+import { useContext } from 'react';
+import { ThemeContext } from '../Context/ThemeContext';
 
-const FloatingActionButtonComponent = ({ toggle, setType }) => {
+const FloatingActionButtonComponent = ({ toggle, setType, setDaisyType }) => {
     // const [type, setType] = React.useState(null);
+
+    const { theme } = useContext(ThemeContext)
 
     const handleCreateItemClick = (itemType) => {
         setType(itemType);
@@ -15,29 +19,60 @@ const FloatingActionButtonComponent = ({ toggle, setType }) => {
         toggle();
     };
 
-    return (
-        <SpeedDial
-            ariaLabel="SpeedDial basic example"
-            sx={{ position: 'fixed', bottom: 30, right: 25 }}
-            icon={<SpeedDialIcon />}
-        >
-            <SpeedDialAction
-                icon={<NewspaperIcon />}
-                tooltipTitle="Create Project"
-                onClick={() => handleCreateItemClick('createproject')}
-            />
-            <SpeedDialAction
-                icon={<TaskIcon />}
-                tooltipTitle="Create Task"
-                onClick={() => handleCreateItemClick('createtask')}
-            />
-            <SpeedDialAction
-                icon={<ArticleIcon />}
-                tooltipTitle="Create Subtask"
-                onClick={() => handleCreateItemClick('createsubtask')}
-            />
-        </SpeedDial>
-    );
-};
+    const handleItemClick = (itemType) => {
+        setDaisyType(itemType);
+        // setParentType(itemType)
+        toggle();
+    };
 
+    return (
+        <>
+            {theme === "theme1" ? (
+                <SpeedDial
+                    ariaLabel="SpeedDial basic example"
+                    sx={{ position: 'fixed', bottom: 30, right: 25 }}
+                    icon={<SpeedDialIcon />}
+                >
+                    <SpeedDialAction
+                        icon={<NewspaperIcon />}
+                        tooltipTitle="Create Project"
+                        onClick={() => handleCreateItemClick('createproject')}
+                    />
+                    <SpeedDialAction
+                        icon={<TaskIcon />}
+                        tooltipTitle="Create Task"
+                        onClick={() => handleCreateItemClick('createtask')}
+                    />
+                    <SpeedDialAction
+                        icon={<ArticleIcon />}
+                        tooltipTitle="Create Subtask"
+                        onClick={() => handleCreateItemClick('createsubtask')}
+                    />
+                </SpeedDial>
+            ) : (
+                <SpeedDial
+                    ariaLabel="SpeedDial basic example"
+                    sx={{ position: 'fixed', bottom: 30, right: 25 }}
+                    icon={<SpeedDialIcon />}
+                >
+                    <SpeedDialAction
+                        icon={<NewspaperIcon />}
+                        tooltipTitle="Create Project"
+                        onClick={() => handleItemClick('createproject')}
+                    />
+                    <SpeedDialAction
+                        icon={<TaskIcon />}
+                        tooltipTitle="Create Task"
+                        onClick={() => handleItemClick('createtask')}
+                    />
+                    <SpeedDialAction
+                        icon={<ArticleIcon />}
+                        tooltipTitle="Create Subtask"
+                        onClick={() => handleItemClick('createsubtask')}
+                    />
+                </SpeedDial>
+            )}
+        </>
+    )
+}
 export default FloatingActionButtonComponent;
