c66d85d82a8c8f89205f7cce82f8ac2ba7cca40a
Adithya S Nair <adithyasnair00@gmail.com>
2024-02-20 11:08:52 +0530

    merge conflict fixed for theme color change

--- client/src/Pages/Common/GanttChart.jsx
+++ client/src/Pages/Common/GanttChart.jsx
@@ -1,45 +1,12 @@
-import React from 'react';
-import { Gantt, Task } from 'gantt-task-react';
-import 'gantt-task-react/dist/index.css';
+import React from 'react'
+import TreeViewComponent from '../../Components/TreeViewComponent'
 
-const MyGanttChart = () => {
-  const tasks = [
-    {
-      start: new Date(2020, 1, 1),
-      end: new Date(2020, 1, 2),
-      name: 'Idea',
-      id: 'Task 0',
-      type: 'task',
-      progress: 45,
-      isDisabled: true,
-      styles: { progressColor: '#ffbb54', progressSelectedColor: '#ff9e0d' },
-      children: [
-        {
-          start: new Date(2020, 1, 1),
-          end: new Date(2020, 1, 1, 12),
-          name: 'Subtask 1',
-          id: 'Subtask 1',
-          type: 'task',
-          progress: 20,
-          isDisabled: true,
-          styles: { progressColor: '#ffbb54', progressSelectedColor: '#ff9e0d' },
-        },
-        {
-          start: new Date(2020, 1, 1, 12),
-          end: new Date(2020, 1, 2),
-          name: 'Subtask 2',
-          id: 'Subtask 2',
-          type: 'task',
-          progress: 70,
-          isDisabled: true,
-          styles: { progressColor: '#ffbb54', progressSelectedColor: '#ff9e0d' },
-        },
-      ],
-    },
-    // Add more tasks here if needed
-  ];
+const GanttChart = () => {
+    return (
+        <div>
+            <TreeViewComponent />
+        </div>
+    )
+}
 
-  return <Gantt tasks={tasks} />;
-};
-
-export default MyGanttChart;
+export default GanttChart;
