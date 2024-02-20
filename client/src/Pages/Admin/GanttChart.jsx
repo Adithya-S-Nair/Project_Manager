42e1922466fd29cc4249e0d19b3afa1e9a9b3959
p111222 <princejainpre.ram@gmail.com>
2024-02-20 15:40:28 +0530

    api getallprojectdetails

--- client/src/Pages/Admin/GanttChart.jsx
+++ client/src/Pages/Admin/GanttChart.jsx
@@ -1,74 +1,31 @@
 import React, { useState } from 'react'
 import TreeViewComponent from '../../Components/TreeViewComponent'
+import { useQuery } from 'react-query';
+import { makeRequest } from '../../Axios';
+import CircularProgress from '@mui/material/CircularProgress';
 
 const GanttChart = () => {
-  const [treeData, setTreeData] = useState([
-    {
-      "project_id": 1,
-      "project_name": "Sample Project 1",
-      "project_start_date": "2024-01-31T18:30:00.000Z",
-      "project_end_date": "2024-02-27T18:30:00.000Z",
-      "tasks": [
-        {
-          "task_id": 4,
-          "task_name": "sample 1",
-          "task_start_date": "2024-02-08T18:30:00.000Z",
-          "task_end_date": "2024-02-18T18:30:00.000Z",
-          "subtasks": [
-            {
-              "subtask_id": 2,
-              "subtask_name": "Sample Subtask ",
-              "subtask_start_date": "2024-02-04T18:30:00.000Z",
-              "subtask_end_date": "2024-02-09T18:30:00.000Z"
-            }
-          ]
-        },
-        {
-          "task_id": 5,
-          "task_name": "Sample Task 2",
-          "task_start_date": "2024-02-09T18:30:00.000Z",
-          "task_end_date": "2024-02-19T18:30:00.000Z",
-          "subtasks": [
-            {
-              "subtask_id": 3,
-              "subtask_name": "Sample Subtask 2",
-              "subtask_start_date": "2024-02-04T18:30:00.000Z",
-              "subtask_end_date": "2024-02-09T18:30:00.000Z"
-            }
-          ]
-        },
-        {
-          "task_id": 6,
-          "task_name": "Sample Task 3",
-          "task_start_date": "2024-02-09T18:30:00.000Z",
-          "task_end_date": "2024-02-19T18:30:00.000Z",
-          "subtasks": []
-        }
-      ]
-    },
-    {
-      "project_id": 2,
-      "project_name": "Sample Project 2",
-      "project_start_date": "2024-02-29T18:30:00.000Z",
-      "project_end_date": "2024-03-30T18:30:00.000Z",
-      "tasks": [
-        {
-          "task_id": 14,
-          "task_name": "Sample Task 20",
-          "task_start_date": "2024-02-09T18:30:00.000Z",
-          "task_end_date": "2024-02-19T18:30:00.000Z",
-          "subtasks": []
-        }
-      ]
-    },
-    {
-      "project_id": 3,
-      "project_name": "Sample Project 3",
-      "project_start_date": "2024-03-31T18:30:00.000Z",
-      "project_end_date": "2024-04-29T18:30:00.000Z",
-      "tasks": []
-    }
-  ])
+  const [treeData, setTreeData] = useState([])
+
+  const { data: projectDetails, error: projectDetailsError, isLoading: projectDetailsLoading } = useQuery(['projectDetails'], async () => {
+    const response = await makeRequest.get(`/project/getprojectdetails`);
+    setTreeData(response.data);
+    // return response.data;
+});
+
+if (projectDetailsLoading) {
+  return (
+      <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
+          <CircularProgress />
+      </div>
+  );
+}
+
+if (projectDetailsError) {
+  console.error('Error fetching data:', projectDetailsError);
+  return <div>Error fetching data</div>;
+}
+
   return (
     <div>
       <TreeViewComponent treeData={treeData} />
