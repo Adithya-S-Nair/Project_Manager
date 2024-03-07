dd80a4ab2673cd65f6cc9bc43ee551aec7b66394
Adithya S Nair <adithyasnair00@gmail.com>
2024-03-07 15:57:54 +0530

    personal task apis done

--- server/controllers/personalTaskController.js
+++ server/controllers/personalTaskController.js
@@ -1,19 +1,53 @@
 import { db } from '../connections/mysql.js';
+import moment from 'moment';
 
 export const createPersonalTask = (req, res) => {
-    const query = `INSERT INTO personal_task VALUES(?)`
+    const empFetchQery = "SELECT * FROM employee WHERE "
+    const query = `INSERT INTO personal_task (
+        employee_id,
+        project_id,
+        personal_task_name,
+        Priority,
+        personal_task_description,
+        planned_start_date,
+        planned_end_date,
+        actual_start_time,
+        actual_end_time,
+        planned_budget,
+        actual_budget,
+        status
+      )
+      SELECT
+        e.employee_id,
+        ? AS project_id,
+        ? AS personal_task_name,
+        ? AS priority,
+        ? AS personal_task_description,
+        ? AS planned_start_date,
+        ? AS planned_end_date,
+        ? AS actual_start_time,
+        ? AS actual_end_time,
+        ? AS planned_budget,
+        ? AS actual_budget,
+        ? AS status
+      FROM employee e
+      WHERE e.user_account_id = ?      
+    `;
     const values = [
+        req.body.projectId,
         req.body.personalTaskName,
-        req.body.personalTaskDesc,
         req.body.priority,
+        req.body.personalTaskDescription,
         req.body.plannedStartDate,
         req.body.plannedEndDate,
         req.body.actualStartTime,
         req.body.actualEndTime,
         req.body.plannedBudget,
-        req.body.actualBudget
+        req.body.actualBudget,
+        req.body.status,
+        req.userId,
     ]
-    db.query(query, [values], (err, data) => {
+    db.query(query, values, (err, data) => {
         if (err) {
             console.error(err);
             return res.status(500).json({ error: "Error creating task" });
@@ -27,8 +61,8 @@ export const getPersonalTasks = (req, res) => {
             FROM personal_task pt
             JOIN employee e ON pt.employee_id = e.employee_id
             JOIN users u ON e.user_account_id = u.user_id
-            WHERE u.user_id = ? AND pt.is_deleted = 0`
-    db.query(query, [req.user_id], (err, data) => {
+            WHERE u.user_id = ? AND project_id = ? AND pt.is_deleted = 0`
+    db.query(query, [req.userId, req.params.projectId], (err, data) => {
         if (err) return res.status(500).json(err);
 
         if (data.length === 0) {
@@ -39,54 +73,38 @@ export const getPersonalTasks = (req, res) => {
     })
 }
 
-export const updatePersonalTask = (req,res) => {
-    const taskId = req.params.taskId;
-
-    if (req.body.employee_name) {
-        const employeeName = req.body.employee_name;
-        console.log(`employee name - ${employeeName}`);
-        db.query('SELECT * FROM assigned WHERE task_id = ?', [taskId], (err, existingAssignment) => {
-            console.log(existingAssignment.length);
-            if (existingAssignment.length > 0) {
-                db.query('UPDATE assigned SET employee_id = (SELECT employee_id FROM employee WHERE employee_name = ?) WHERE task_id = ?', [employeeName, taskId]);
-            } else {
-                db.query('INSERT INTO assigned (project_id, task_id, employee_id) VALUES (?, ?, (SELECT employee_id FROM employee WHERE employee_name = ?))',
-                    [req.body.project_id, taskId, employeeName]); // replace projectId and roleId with the appropriate values
-            }
-        });
-    }
+export const updatePersonalTask = (req, res) => {
+    const personalTaskId = req.params.personalTaskId;
 
     const query = `
-            UPDATE task
+            UPDATE personal_task
             SET
-            task_name = ?,
-            project_id = ?,
+            personal_task_name = ?,
             Priority = ?,
-            task_description = ?,
+            personal_task_description = ?,
             planned_start_date = ?,
             planned_end_date = ?,
-            planned_budget = ?,
             actual_start_time = ?,
             actual_end_time = ?,
+            planned_budget = ?,
             actual_budget = ?,
             status = ?
             WHERE
-            task_id = ?
+            personal_task_id = ?
             And is_deleted = 0`
 
     const values = [
-        req.body.task_name,
-        req.body.project_id,
+        req.body.personalTaskName,
         req.body.Priority,
-        req.body.task_description,
-        moment(req.body.planned_start_date).format('YYYY-MM-DD'),
-        moment(req.body.planned_end_date).format('YYYY-MM-DD'),
-        req.body.planned_budget,
-        moment(req.body.actual_start_time).format('YYYY-MM-DD'),
-        moment(req.body.actual_end_time).format('YYYY-MM-DD'),
-        req.body.actual_budget,
+        req.body.personalTaskDesc,
+        moment(req.body.plannedStartDate).format('YYYY-MM-DD'),
+        moment(req.body.plannedEndDate).format('YYYY-MM-DD'),
+        moment(req.body.actualStartTime).format('YYYY-MM-DD'),
+        moment(req.body.actualEndTime).format('YYYY-MM-DD'),
+        req.body.plannedBudget,
+        req.body.actualBudget,
         req.body.status,
-        taskId
+        personalTaskId
     ];
 
     db.query(query, values, (err, data) => {
@@ -96,7 +114,7 @@ export const updatePersonalTask = (req,res) => {
     })
 }
 
-export const deletePersonalTask = (req,res) => {
+export const deletePersonalTask = (req, res) => {
     const personalTaskIds = req.params.personalTaskIds.split(',');
     if (!Array.isArray(personalTaskIds) || personalTaskIds.length === 0) {
         return res.status(400).json({ error: 'Invalid task IDs provided' });
@@ -104,7 +122,7 @@ export const deletePersonalTask = (req,res) => {
 
     const query = `UPDATE personal_task 
         SET is_deleted = 1
-        WHERE task_id IN (?)`;
+        WHERE personal_task_id IN (?)`;
 
     db.query(query, [personalTaskIds], (error, results) => {
         if (error) {
@@ -114,4 +132,26 @@ export const deletePersonalTask = (req,res) => {
 
         res.status(200).json({ message: 'Personal Tasks deleted successfully' });
     });
+}
+
+export const getProjectPriorityBasedPersonalTask = (req, res) => {
+    const { projectId, priority } = req.params;
+
+    const query = ' SELECT * FROM personal_task WHERE project_id = ? AND Priority = ? AND is_deleted = 0';
+    const value = [projectId, priority];
+    db.query(query, value, (err, data) => {
+        if (err) return res.status(500).json(err);
+
+        if (data.length === 0) {
+            return res.status(404).json({ msg: "No data found" });
+        }
+        data.forEach(row => {
+            row.planned_start_date = moment(row.planned_start_date).format('YYYY-MM-DD');
+            row.planned_end_date = moment(row.planned_end_date).format('YYYY-MM-DD');
+            row.actual_start_time = moment(row.actual_start_time).format('YYYY-MM-DD');
+            row.actual_end_time = moment(row.actual_end_time).format('YYYY-MM-DD');
+        });
+
+        return res.status(200).json(data)
+    })
 }
--- server/routes/personalTaskRoute.js
+++ server/routes/personalTaskRoute.js
@@ -1,13 +1,14 @@
 import express from 'express';
 import checkAuth from '../middlewares/checkAuth.js';
-import { createPersonalTask, getPersonalTasks, updatePersonalTask, deletePersonalTask } from '../controllers/personalTaskController.js';
+import { createPersonalTask, getPersonalTasks, updatePersonalTask, deletePersonalTask, getProjectPriorityBasedPersonalTask } from '../controllers/personalTaskController.js';
 
 const router = express.Router();
 
 // Define personal task routes
 router.post('/createpersonaltask', checkAuth, createPersonalTask);
-router.get('/getpersonaltasks', checkAuth, getPersonalTasks);
+router.get('/getpersonaltasks/:projectId', checkAuth, getPersonalTasks);
 router.patch('/updatepersonaltask/:personalTaskId', checkAuth, updatePersonalTask);
 router.delete('/deletepersonaltask/:personalTaskIds', checkAuth, deletePersonalTask);
+router.get('/getprojectprioritybasedpersonaltask/:projectId/:priority', checkAuth, getProjectPriorityBasedPersonalTask)
 
 export default router;
